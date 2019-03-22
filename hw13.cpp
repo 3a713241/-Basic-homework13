44478ef2eb53b247942105fc569f90c5b0b2064b
3a713241 <48574949+3a713241@users.noreply.github.com>
2019-03-22 10:27:11 +0800

    Add files via upload

---
+++ hw13.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int main()
+{
+    int b,g;
+	printf("請輸入男生年紀: \n");
+	scanf("%d",&b);
+	printf("請輸入女生年紀: \n");
+	scanf("%d",&g);
+	
+	
+   if(b>=18&&g>=16)
+   printf("恭喜你們可以結婚\n");
+   else
+   printf("哭哭不能結婚QQ\n");
+   system("PAUSE");
+   return 0;
+    
+   
+   
+   
+	
+	
+
+	
+}
